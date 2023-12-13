2faa1e2d01fb0155e965bfc603510ee3badd214d
Yonas yalew <yonasyalewwerke@gmail.com>
2023-12-13 00:07:02 -0800

    getline

---
+++ memtest/getline.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *_getline() {
+    char *line = NULL;
+    size_t line_size = 0;
+    ssize_t chars_read;
+    size_t current_size = 0;
+
+    while ((chars_read = getline(&line, &line_size, stdin)) != -1) {
+        current_size += chars_read;
+
+        // Check if the line is complete (ends with a newline)
+        if (line[chars_read - 1] == '\n') {
+            // Remove the newline character
+            line[chars_read - 1] = '\0';
+            break;
+        }
+
+        // Resize the buffer to accommodate more input
+        char *temp = realloc(line, current_size + line_size);
+        if (temp == NULL) {
+            fprintf(stderr, "Memory reallocation failed\n");
+            free(line);
+            return NULL;
+        }
+        line = temp;
+    }
+
+    return line;
+}
+
+int main() {
+    printf("Enter a line: ");
+    fflush(stdout);
+
+    char *line = _getline();
+    if (line != NULL) {
+        printf("Input: %s\n", line);
+        free(line);
+    }
+
+    return 0;
+}
